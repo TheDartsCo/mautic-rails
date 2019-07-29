dadde057084ee111c74fbf463d73685222366a13
Hossam Eldeen Hawary <Hossam-Hawary@users.noreply.github.com>
2019-07-29 10:55:06 +0200

    Remove the ID from the create URL
    This ID in the create URL is not needed as there is no create endpoint includes an ID in it 
    only like "POST resources/new" for create

--- lib/mautic/model.rb
+++ lib/mautic/model.rb
@@ -46,7 +46,7 @@ module Mautic
 
       def create(connection, params = {})
         begin
-          json = connection.request(:post, "api/#{endpoint}/#{id && "#{id}/"}new", { body: params })
+          json = connection.request(:post, "api/#{endpoint}/new", { body: params })
 
           instance = new(connection, json[field_name.singularize])
         rescue ValidationError => e
